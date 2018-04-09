5ef9cbbe48b90aad127ce47548f62cfb7677ac8b
nhanuscin <hanusn1@unlv.nevada.edu>
2018-04-08 21:28:19 -0700

    updated info

--- DA_Midterm/DA_Midterm/DA_Midterm/main.c
+++ DA_Midterm/DA_Midterm/DA_Midterm/main.c
@@ -8,11 +8,7 @@
 #define F_CPU 8000000UL
 #define UBRR_115200 3 // for 8Mhz with 8.5% error
 
-#define Domain "api.thingspeak.com"
-#define API_Write_Key "PG5YKHOM60E8XQRI"
 #define Channel_ID "461798"
-#define SSID ""
-#define Password ""
 
 
 #include <avr/io.h>
@@ -21,53 +17,9 @@
 #include <avr/interrupt.h>
 #include <stdint.h>
 
-
-// Function Declarations
-void read_adc(void);
-void adc_init(void);
-void USART_init( unsigned int ubrr );
-void USART_tx_string( char *data );
 volatile unsigned int adc_temp;
-char outs[30];
+char outs[20];
 
-
-int main(void)
-{
-	adc_init(); 						//Initialize the ADC (Analog / Digital Converter)
-	USART_init(UBRR_115200); 			//Initialize the USART (RS232 interface)
-	_delay_ms(125); 					//wait a bit
-	snprintf(outs,sizeof(outs),"AT\r\n");
-	USART_tx_string(outs);
-	_delay_ms(2000);  
-	snprintf(outs,sizeof(outs),"AT+CWMODE=3\r\n");  //wifi mode
-	USART_tx_string(outs);
-	_delay_ms(2000);
-	snprintf(outs,sizeof(outs),"AT+CIPMUX=0\r\n");	//single connection
-	USART_tx_string(outs);
-	_delay_ms(2000);
-	snprintf(outs,sizeof(outs),"AT+CIPMODE=0\r\n");	//normal mode
-	USART_tx_string(outs);
-	_delay_ms(2000);
-	snprintf(outs,sizeof(outs),"AT+CWJAP=\"%s\",\"%s\"\r\n", SSID, Password);		//connect to wifi network
-	USART_tx_string(outs);
-	_delay_ms(2000);
-	snprintf(outs,sizeof(outs),"AT+CPISTART=\"TCP\",\"%s\",80\r\n",Domain);			//connect to thingspeak
-	USART_tx_string(outs);
-	_delay_ms(2000);
-
-	
-	
-	while(1)
-	{
-		read_adc();
-		snprintf(outs,sizeof(outs),"GET /update?api_key=%s&field1=1\r\n",API_Write_Key);		//send value
-		USART_tx_string(outs);
-		_delay_ms(150);
-		snprintf(outs,sizeof(outs),"AT+CISEND=%3d\r\n",adc_temp);
-		USART_tx_string(outs);
-		_delay_ms(15000);			//wait 15 seconds
-	}
-}
 
 void adc_init(void)
 {
@@ -96,7 +48,7 @@ void read_adc(void)
 	while (i--)
 	{
 		ADCSRA |= (1<<ADSC);				//start the conversion
-		while((ADCSRA & (1<<ADIF)) == 0);	//wait for conversion to finish                    
+		while((ADCSRA & (1<<ADIF)) == 0);	//wait for conversion to finish
 		adc_temp += ADCH*2;					//get temp value
 		_delay_ms(50);						//wait a bit
 	}
@@ -116,7 +68,7 @@ void USART_init( unsigned int ubrr )
 /* SEND A STRING TO THE RS-232*/
 void USART_tx_string( char *data )
 {
-	while ((*data != '\0'))					
+	while ((*data != '\0'))
 	{
 		while (!(UCSR0A & (1 <<UDRE0)));	//wait for the transmit buffer to empty
 		UDR0 = *data;						//put the data into the empty buffer, which sends the data
@@ -124,4 +76,77 @@ void USART_tx_string( char *data )
 		data++;
 	}
 }
+
+void usart_send( unsigned char ascii)
+{
+	while(!(UCSR0A & (1<<UDRE0)));
+	UDR0 = ascii;
+}
+unsigned char usart_receive(void)
+{
+	while (!(UCSR0A & (1<<RXC0)));
+	return UDR0;
+}
+void send_AT( unsigned char message[])
+{
+	unsigned char i=0;
+	while(message[i] != '\0')
+	{
+		usart_send(message[i]); // This sends data to esp
+		i++;
+	}
+}
+
+
+int main(void)
+{
+	unsigned char AT[] = "AT\r\n";
+	unsigned char CWMODE[] = "AT+CWMODE=3\r\n";
+	unsigned char CWJAP[] = "AT+CWJAP= \"SSID\",\"PASSWORD\"\r\n";
+	
+	unsigned char CIPMUX[] = "AT+CIPMUX=0\r\n";
+	//unsigned char CIPMUX[] = "AT+CIPMUX=1\r\n";
+	
+	unsigned char CIPSTART[] = "AT+CIPSTART=0,\"TCP\",\"api.thingspeak.com\",80\r\n";
+	
+	unsigned char CIPSEND[] = "AT+CIPSEND=45\r\n";
+	//unsigned char CIPSEND[] = "AT+CIPSEND=0,110\r\n";
+	
+	//unsigned char GET_DATA[] = "GET https://api.thingspeak.com/apps/thinghttp/send_request?api_key=D6UQ4AHE1FSE4808\r\n";
+	
+	unsigned char SEND_DATA[] = "GET /update?key=PG5YKHOM60E8XQRI&field1=";
+	//unsigned char SEND_DATA[] = "GET https://api.thingspeak.com/update?api_key=PG5YKHOM60E8XQRI=50\r\n";
+	
+	
+	adc_init(); 						//Initialize the ADC (Analog / Digital Converter)
+	USART_init(UBRR_115200); 			//Initialize the USART (RS232 interface)
+	_delay_ms(125); 					//wait a bit
+	
+	_delay_ms(200);
+	send_AT(AT);					//send AT commands
+	_delay_ms(2000);
+	send_AT(CWMODE);				//initialize wifi mode
+	_delay_ms(2000);
+	send_AT(CIPMUX);				//single connection
+	_delay_ms(2000);
+	send_AT(CWJAP);					//connect to wifi network
+	_delay_ms(2000);
+	send_AT(CIPSTART);				//connect to thingspeak server
+	_delay_ms(2000);
+
+	while(1)
+	{
+		read_adc();
+		snprintf(outs,sizeof(outs),"%3d\r\n", adc_temp);		// convert to string
+		send_AT(CIPSEND);										// send number of bytes
+		_delay_ms(2000);
+		send_AT(SEND_DATA);										// send data command
+		_delay_ms(2000);
+		USART_tx_string(outs);									// send converted value
+		_delay_ms(15000);
+	
+	}
+}
+
+
 
